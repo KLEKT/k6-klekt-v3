88af4d9242e3a6dfe266951cc8e30563fa1a22b8
Chris <chris.sewell@buildcircle.co.uk>
2023-07-10 18:32:42 +0100

    fix node verson

--- tests/script.js
+++ tests/script.js
@@ -1,8 +1,9 @@
 import http from 'k6/http';
+import { check } from 'k6';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('https://test.k6.io');
+  const res = http.get('https://test.k6.io');
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
